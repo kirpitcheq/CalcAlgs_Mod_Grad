61abd5c0a3aec29d2bb0e1b084a2a3858921a883
kirpitcheq <kirpitcheq@yandex.ru>
2023-11-04 14:53:24 +0300

    [ModGrad][lw2][rungeode] fix wrong with missing literals (double values interp like int)

--- ModGrad/lw2_ODE_RungeAll/mod_lw2_lib/inc/KPEq/rungeode.hpp
+++ ModGrad/lw2_ODE_RungeAll/mod_lw2_lib/inc/KPEq/rungeode.hpp
@@ -52,8 +52,8 @@ namespace KPEq
                 TVal alpha = 1.0/2.0;
                 TVal k1 = func1(xlast,ylast,zlast); // ? no step mul?
                 TVal j1 = func2(xlast,ylast,zlast); // ? no step mul?
-                TVal k2 = step * func1(xlast + step/(2*alpha), ylast + step/(2*alpha) * k1, zlast + step/(2*alpha) * j1);
-                TVal j2 = step * func2(xlast + step/(2*alpha), ylast + step/(2*alpha) * k1, zlast + step/(2*alpha) * j1);
+                TVal k2 = step * func1(xlast + step/(2.0*alpha), ylast + step/(2.0*alpha) * k1, zlast + step/(2.0*alpha) * j1);
+                TVal j2 = step * func2(xlast + step/(2.0*alpha), ylast + step/(2.0*alpha) * k1, zlast + step/(2.0*alpha) * j1);
                 xlast+=step;
                 return {
                     ylast + (1-alpha)*k1 + alpha*k2,
@@ -63,15 +63,19 @@ namespace KPEq
             std::pair<TVal,TVal> calcNextByRunge4(TVal & xlast, TVal ylast, TVal zlast){
                 TVal k1 = step * func1(xlast,ylast,zlast);
                 TVal j1 = step * func2(xlast,ylast,zlast);
-                TVal k2 = step * func1(xlast+step/2,ylast+k1/2, zlast+j1/2);
-                TVal j2 = step * func2(xlast+step/2,ylast+k1/2, zlast+j1/2);
-                TVal k3 = step * func1(xlast+step/2,ylast+k2/2, zlast+j2/2);
-                TVal j3 = step * func2(xlast+step/2,ylast+k2/2, zlast+j2/2);
+
+                TVal k2 = step * func1(xlast+step/2.0,ylast+k1/2.0, zlast+j1/2.0);
+                TVal j2 = step * func2(xlast+step/2.0,ylast+k1/2.0, zlast+j1/2.0);
+
+                TVal k3 = step * func1(xlast+step/2.0,ylast+k2/2.0, zlast+j2/2.0);
+                TVal j3 = step * func2(xlast+step/2.0,ylast+k2/2.0, zlast+j2/2.0);
+
                 TVal k4 = step * func1(xlast+step,ylast+k3,zlast+j3);
                 TVal j4 = step * func2(xlast+step,ylast+k3,zlast+j3);
+
                 xlast += step;
-                auto y_res = ylast + (k1 + 2*k2 + 2*k3 + k4) / 6 ;
-                auto z_res = zlast + (j1 + 2*j2 + 2*j3 + j4) / 6;
+                auto y_res = ylast + (k1 + 2.0*k2 + 2.0*k3 + k4) / 6.0 ;
+                auto z_res = zlast + (j1 + 2.0*j2 + 2.0*j3 + j4) / 6.0;
                 return {
                     y_res,
                     z_res
