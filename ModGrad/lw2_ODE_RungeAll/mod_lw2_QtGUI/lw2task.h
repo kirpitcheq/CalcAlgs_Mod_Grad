8c7e2eb718c30d608eb1cd456140abf3d6a1fe38
kirpitcheq <kirpitcheq@yandex.ru>
2023-11-05 12:31:55 +0300

    [ModGrad][lw2][QtGUI] move logic to class LW2Task from mainwindow.cpp

---
+++ ModGrad/lw2_ODE_RungeAll/mod_lw2_QtGUI/lw2task.h
@@ -0,0 +1,112 @@
+﻿#ifndef LW2TASK_H
+#define LW2TASK_H
+#include <KPEq/newtpoly.hpp>
+#include <KPEq_Q_Libs/itemmodel.h>
+#include <KPEq/chart.h>
+#include <KPEq/integral.hpp>
+
+#include <KPEq/rungeode.hpp>
+#include <QVXYModelMapper>
+
+#ifdef MOD_LW2_DEBUG
+    #define PRINTTB(VAL) fmt::print("{} {}\t",#VAL,VAL)
+    #define PRINTLN(VAL) fmt::print("{} {}\n",#VAL,VAL)
+    #define PRINTNEWLN() fmt::print("\n")
+#else
+    #define PRINTTB(VAL) ;
+    #define PRINTLN(VAL) ;
+    #define PRINTNEWLN() ;
+#endif
+
+class LW2Task
+{
+public:
+    LW2Task(int polypow,
+        double integr_step,
+        KPEq::Interpoll::NewtCnfgEnumC newtsmode,
+        const KPEq::Q::ItemModel &modelinits,
+        const KPEq::Q::ItemModel &modelIT0,
+        const KPEq::Q::ItemModel &modelIm,
+        const KPEq::Q::ItemModel &modelTSigm);
+    void calcODE(double modTime, double modStep, KPEq::Q::ItemModel & results_model, KPEq::Chart & chart);
+private:
+    bool isCachable = true;
+    /* init values */
+    double R, l_p, L_k, C_k, R_k, U_co, I_0, T_w, integr_step;
+    int polypow;
+    KPEq::Interpoll::NewtCnfgEnumC newtsmode;
+    KPEq::Interpoll::SrcNodesType tabledata_T0I, tabledata_mI, tabledata_SigmT;
+    KPEq::Interpoll::NewtPoly newt_T0byI,newt_mbyI,newt_SigmByT; // interface need but full rework class?
+    /* ----------------------------------------------------------------- */
+
+    /* this need set by common table.. but count of values difference! */
+    /* is need caching values ? */
+
+
+    KPEq::Interpoll::SrcNodesType nodesFromModels(const KPEq::Q::ItemModel & model, int first_i, int second_i);
+
+
+    /* ************************************************************* */
+    /* init interpollation objects */
+
+    double T (double T0, double m, double z) { return (T0 - (T_w - T0) * std::pow(z, m)); }
+
+    std::pair<double,double> interp_T0_m_byI(double I)
+    {
+        if(newt_T0byI.setConfig(abs(I), polypow) < 0)
+            throw std::invalid_argument("newtbyZ wrong cnfg");
+        auto T0 = newt_T0byI.calc();
+        if(!T0.has_value())
+            throw std::runtime_error("T0 hasn't value");
+        PRINTTB(T0.value());
+
+        if(newt_mbyI.setConfig(abs(I), polypow) < 0)
+            throw std::invalid_argument("newtbyZ wrong cnfg");
+        auto m = newt_mbyI.calc();
+        if(!m.has_value())
+            throw std::runtime_error("m hasn't value");
+        PRINTTB(m.value());
+
+        return {T0.value(),m.value()};
+    };
+
+    double sigm (double T)
+    {
+        if(newt_SigmByT.setConfig(T, polypow) < 0)
+            throw std::invalid_argument("newt_SigmByT wrong cnfg");
+
+        auto sigma = newt_SigmByT.calc();
+        if(!sigma.has_value())
+            throw std::runtime_error("sigma hasn't value");
+        PRINTTB(sigma.value());
+
+        return sigma.value();
+    };
+
+    double T0 ;
+    double m ;
+    std::tuple<double,double,double> Rp_T0_m_tuple;
+    auto R_p (double I)
+    {
+        auto pair_T0_m = interp_T0_m_byI(I);
+        T0 = pair_T0_m.first;
+        m = pair_T0_m.second;
+        KPEq::IntegrandFunc integrfunc = [this](double z) -> double {
+            auto iterIntegr = sigm(T(T0,m,z))*z;
+            PRINTTB(iterIntegr);
+            return iterIntegr;
+        };
+
+        KPEq::Integral integr(integrfunc, 0, 1);
+        auto integr_sigmaByTByT0nZ_mul_z = integr.calcBySympson(integr_step);
+        PRINTLN(integr_sigmaByTByT0nZ_mul_z);
+
+        auto R_p = ( l_p / (2 * M_PI * R * R * (integr_sigmaByTByT0nZ_mul_z))); PRINTTB(R_p);
+
+        Rp_T0_m_tuple = std::tuple(R_p,T0,m);
+        return R_p;
+    };
+
+};
+
+#endif // LW2TASK_H
--- ModGrad/lw2_ODE_RungeAll/mod_lw2_QtGUI/mainwindow.cpp
+++ ModGrad/lw2_ODE_RungeAll/mod_lw2_QtGUI/mainwindow.cpp
@@ -10,17 +10,9 @@
 #include <numbers>
 #include <QVXYModelMapper>
 #include <memory>
+#include "lw2task.h"
 
 //#define MOD_LW2_DEBUG
-#ifdef MOD_LW2_DEBUG
-    #define PRINTTB(VAL) fmt::print("{} {}\t",#VAL,VAL)
-    #define PRINTLN(VAL) fmt::print("{} {}\n",#VAL,VAL)
-    #define PRINTNEWLN() fmt::print("\n")
-#else
-    #define PRINTTB(VAL) ;
-    #define PRINTLN(VAL) ;
-    #define PRINTNEWLN() ;
-#endif
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -116,6 +108,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     results_model = new KPEq::Q::ItemModel(this);
     ui->tableV_OutData->setModel(results_model); // тут по идее м.б. замедление т.к. данные не передаются... они копируются
+
+
 }
 
 MainWindow::~MainWindow()
@@ -126,194 +120,14 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_calculateBtn_clicked()
 {
-    bool isCachable = true;
-    double R   = (*initdata_model)[0][0];//Tk_sigma_model[0][1];
-    double l_p = (*initdata_model)[1][0]; //sm -> m
-    double L_k = (*initdata_model)[2][0]; //from spin or TexBox
-    double C_k = (*initdata_model)[3][0];
-    double R_k = (*initdata_model)[4][0]; //from spin or TexBox
-    double U_co= (*initdata_model)[5][0];
-    double I_0 = (*initdata_model)[6][0];
-    double T_w = (*initdata_model)[7][0];
-    /* init values */
-    double I = I_0;
-    double U = U_co;
-
-    int polypow = ui->polypowSpin->value(); //from spin polypow interpollation
+    int polypow = ui->polypowSpin->value();
     double integr_step = ui->integstepSpin->value();
-    /* ----------------------------------------------------------------- */
+    KPEq::Interpoll::NewtCnfgEnumC newtsmode = KPEq::Interpoll::NewtCnfgEnumC::NORMAL;
 
-    /* this need set by common table.. but count of values difference! */
-    /* is need caching values ? */
+    LW2Task lw2task1(polypow, integr_step, newtsmode, *initdata_model, *I_T0_m_data_model, *I_T0_m_data_model, *Tk_sigma_model);
 
-    KPEq::Interpoll::SrcNodesType tabledata_T0I;
-    KPEq::Interpoll::SrcNodesType tabledata_mI;
-    for(auto i = 0; i < this->I_T0_m_data_model->rowCount(); i++){
-        auto I  = (*I_T0_m_data_model)[i][0];
-        auto T0 = (*I_T0_m_data_model)[i][1];
-        auto m  = (*I_T0_m_data_model)[i][2];
-        tabledata_T0I.push_back	({I,T0});
-        tabledata_mI.push_back	({I,m});
-    }
-    KPEq::Interpoll::SrcNodesType tabledata_SigmT;
-    for(auto i = 0; i < this->Tk_sigma_model->rowCount(); i++){
-        auto T  	= (*Tk_sigma_model)[i][0];
-        auto sigma  = (*Tk_sigma_model)[i][1];
-        tabledata_SigmT.push_back({T,sigma});
-    }
-
-    /* ************************************************************* */
-    /* init interpollation objects */
-
-    static auto T = [T_w](double T0, double m, double z)->double{
-        auto T = (T0 - (T_w - T0) * std::pow(z, m));
-        return T;
-    } ;
-
-    auto newtsmode = KPEq::Interpoll::NewtCnfgEnumC::NORMAL;
-//    auto newtsmode = KPEq::Interpoll::NewtCnfgEnumC::LOGARIPHMIC;
-    KPEq::Interpoll::NewtPoly newt_T0byI(tabledata_T0I, newtsmode); // interface need but full rework class?
-    if(newt_T0byI.isWrong())
-        throw std::invalid_argument("wrong table data");
-    KPEq::Interpoll::NewtPoly newt_mbyI(tabledata_mI, newtsmode);
-    if(newt_mbyI.isWrong())
-        throw std::invalid_argument("wrong table data");
-
-    static auto interp_T0_m_byI = [&polypow, &newt_T0byI, &newt_mbyI,T_w](double I) -> std::pair<double,double>
-    {
-        if(newt_T0byI.setConfig(abs(I), polypow) < 0)
-            throw std::invalid_argument("newtbyZ wrong cnfg");
-        auto T0 = newt_T0byI.calc();
-        if(!T0.has_value())
-            throw std::runtime_error("T0 hasn't value");
-        PRINTTB(T0.value());
-
-        if(newt_mbyI.setConfig(abs(I), polypow) < 0)
-            throw std::invalid_argument("newtbyZ wrong cnfg");
-        auto m = newt_mbyI.calc();
-        if(!m.has_value())
-            throw std::runtime_error("m hasn't value");
-        PRINTTB(m.value());
-       
-        return {T0.value(),m.value()};
-    };
-
-
-    KPEq::Interpoll::NewtPoly newt_SigmByT(tabledata_SigmT, newtsmode);
-    if(newt_SigmByT.isWrong())
-        throw std::invalid_argument("wrong table data");
-
-    static auto sigm = [&polypow, & newt_SigmByT](double T)->double
-    {
-        if(newt_SigmByT.setConfig(T, polypow) < 0)
-            throw std::invalid_argument("newt_SigmByT wrong cnfg");
-
-        auto sigma = newt_SigmByT.calc();
-        if(!sigma.has_value())
-            throw std::runtime_error("sigma hasn't value");
-        PRINTTB(sigma.value());
-
-        return sigma.value();
-    };
-
-    std::tuple<double,double,double> Rp_T0_m_tuple;
-    static auto R_p = [l_p, R, integr_step, &Rp_T0_m_tuple](double I) -> double
-    {
-        auto pair_T0_m = interp_T0_m_byI(I);
-        auto T0 = pair_T0_m.first;
-        auto m = pair_T0_m.second;
-        KPEq::IntegrandFunc integrfunc = [T0,m](double z) -> double {
-            auto iterIntegr = sigm(T(T0,m,z))*z;
-            PRINTTB(iterIntegr);
-            return iterIntegr;
-        };
-
-        KPEq::Integral integr(integrfunc, 0, 1);
-        auto integr_sigmaByTByT0nZ_mul_z = integr.calcBySympson(integr_step);
-        PRINTLN(integr_sigmaByTByT0nZ_mul_z);
-
-        auto R_p = ( l_p / (2 * M_PI * R * R * (integr_sigmaByTByT0nZ_mul_z))); PRINTTB(R_p);
-
-        Rp_T0_m_tuple = std::tuple(R_p,T0,m);
-        return R_p;
-    };
-
-    auto di_dt = [R_k, L_k](double t, double I, double U) -> double {
-        auto R_p_ByI = R_p(I);
-        auto result = ( (U - (R_k + R_p_ByI) * I) / L_k );
-        return result;
-    };
-    auto du_dt = [C_k](double t, double I, double U) -> double {
-        auto result = ( -I / C_k );
-        return result;
-    };
-    /* ************************************************************* */
-
-    /* init ODE */
     double modTime = ui->modtimeSpin->value();
     double modStep = ui->modstepSpin->value();
-    double time = 0;
-
-    double RpScale = 1000.0, T0Scale = 0.1, mScale = 10.0;
-    QLineSeries * results_series_I = new QLineSeries(); 	results_series_I->setName("I(t)");
-    QLineSeries * results_series_U = new QLineSeries(); 	results_series_U->setName("U(t)");
-    QLineSeries * results_series_Rp = new QLineSeries(); 	results_series_Rp->setName(QString("Rp(t)*%1").arg(RpScale));
-    QLineSeries * results_series_IRp = new QLineSeries(); 	results_series_IRp->setName(QString("I(t)*Rp(t)"));
-    QLineSeries * results_series_T0 = new QLineSeries(); 	results_series_T0->setName(QString("T0(t)*%1").arg(T0Scale));
-//    QLineSeries * results_series_m = new QLineSeries(); 	results_series_m->setName(QString("m(t)*%1").arg(mScale));
-    QVector<QLineSeries*> series_vect;
-    series_vect << results_series_I << results_series_U << results_series_Rp << results_series_IRp << results_series_T0 ;
-
-    /* calc ODE */
-    KPEq::ODE::ODESysEqua odeSysEqua(di_dt, du_dt, modStep);
-
-    KPEq::Q::Tcont results;
-    results.push_back({time,I,U});
-
-    while(time < modTime)
-    {
-        auto I_U_pair = odeSysEqua.calcNextByRunge1(time, I, U); //here need switch to choise Runge, need realize Runge1
-        I = I_U_pair.first; 	PRINTLN(I);
-        U = I_U_pair.second; 	PRINTLN(U); PRINTNEWLN();
-        if(I!=I || U != U) 
-            break; // is NaN
-
-        /* set data to table */
-        double Rp,T0,m;
-        std::tie(Rp,T0,m) = Rp_T0_m_tuple;
-        results.push_back({time,I,U,Rp*RpScale,I*Rp,T0*T0Scale});
-    }
-
-    (*results_model) << std::move(results);
-    results_model->setHeaderData(0, Qt::Orientation::Horizontal,  "t");
-    results_model->setHeaderData(1, Qt::Orientation::Horizontal,  "I");
-    results_model->setHeaderData(2, Qt::Orientation::Horizontal,  "U");
-    results_model->setHeaderData(3, Qt::Orientation::Horizontal,  QString("Rp*%1").arg(RpScale));
-    results_model->setHeaderData(4, Qt::Orientation::Horizontal,  "I*Rp");
-    results_model->setHeaderData(5, Qt::Orientation::Horizontal,  QString("T0*%1"));
-
-
-    /* add results to chart */
-    bool isFirst = true;
-    int counter = 1;
-        fmt::print("{}\n", counter);
-    chart->removeAllSeries();
-    for(auto s : series_vect)
-    {
-        s->clear();
-        auto map = new QVXYModelMapper(this);
-        map->setXColumn(0);
-        fmt::print("{}\n", counter);
-        map->setYColumn(counter++);
-        map->setModel(results_model);
-        map->setSeries(s);
-        if(isFirst)
-        {
-            this->chart->updateSeries(s);
-            isFirst = false;
-        }
-        else
-            this->chart->addSeries(s);
-        this->chart->createDefaultAxes();
-    }
+    lw2task1.calcODE(modTime,modStep, *results_model, *chart);
 }
+
